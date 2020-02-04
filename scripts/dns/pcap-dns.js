e21d80781e445399e6bcd3832031be9b1782235f
jyri <jyrishamov@gmail.com>
2020-02-04 09:05:11 +0200

    dns-pcap utils updated

--- scripts/dns/pcap-dns.js
+++ scripts/dns/pcap-dns.js
@@ -7,39 +7,61 @@
    
    Tested on newtork layer IPv4, IPv6 and transport layer UDP and TCP. (IPv6 + TCP not tested).
 */
-var offset;  //current position in the dns message 
+var base;	 //start of dns message (pcap packet payload)
+var offset;  //current position in the dns message
 var limit;   //dns message size
 
-function parseDnsMessage(bytes) {
+function AnswerSection() {
+  this.name = "";
+  this.type = 0;
+  this.class = 0;
+  this.ttl = 0;
+  this.rdata = "";
+
+  this.toString = function() {
+	return (
+	  "\n\t\t" + "{" +
+	  	"\n\t\t\t" + "name: " + this.name +
+	  	"\n\t\t\t" + "type: " + this.type.toString() +
+	  	"\n\t\t\t" + "class: " + this.class.toString() +
+	  	"\n\t\t\t" + "ttl: " + this.ttl.toString() +
+	  	"\n\t\t\t" + "rdata: " + this.rdata +
+	  "\n\t\t" + "}"
+	);
+  }
+}
+
+function QuestionSection() {
+  this.qName = "";
+  this.qType = -1;
+  this.qClass = -1;
+
+  this.toString = function() {
+	return (
+	  "\n\t" + "{" +
+	  	"\n\t\t" + "qType: " + this.qType.toString() +
+	  	"\n\t\t" + "qClass: " + this.qClass.toString() +
+	  	"\n\t\t" + "qName: " + this.qName +
+	  "\n\t" + "}"
+	);
+  }
+}
+
+
+function parseDnsMessage(bytes, captureLen, networkType, transportType, srcPort, dstPort) {
 
   // PcapPcaket provides convenience methods for handling pcap packet
-  var packet = new PcapPacket(bytes);
-  if(!packet.hasData() || (packet.getSrcPort()!=53 && packet.getDstPort!=53)){
-	return {txId:-1, 
-			flags:-1,
-			opCode:-1,
-			isQuery:false,
-			isAuthoritative:false,
-			isTruncated:false,
-			recursionDesired:false,
-			recursionAvailable:false,
-			rCode:-1,
-			questionCnt:-1,
-			answerCnt:-1, 
-			authorityCnt:-1, 
-			additionalCnt:-1,
-			queries:[],
-			answers:[]
-		   };	
+  var packet = new PcapPacket(bytes, captureLen, networkType, transportType);
+  if(!packet.hasData() || (srcPort!=53 && dstPort!=53)){
+	return null;
   }
-  var buf = packet.getPayload();
+  var buf = bytes;
+  base = packet.getPayloadOffset();
+  if(transportType == 6) //skip TCP payload len
+  	base += 2;
+  offset = base;
+  limit = packet.size;
 
-  offset = 0;
-  limit = buf.length;
-
-  if(packet.getTransportType() == 6) //skip TCP payload len
-  	offset += 2;
-  
   if(limit >= offset+12) {
 	var txId 			= getShort(buf, offset);  offset += 2;
 	var flags 			= getShort(buf, offset);  offset += 2;
@@ -49,20 +71,21 @@ function parseDnsMessage(bytes) {
 	var additionalRRCnt = getShort(buf, offset);  offset += 2;
   }
   var opCode = (flags>>11)&0xf;
-  
+
   var qArray = [];
   for(var i=0; i<questionCnt; i++) {
 	var q = getQueries(buf);
 	qArray.push(q);
   }
-  
+
   var aArray = [];
   for(var i=0; i<answerRRCnt; i++) {
 	var a = getAnswers(buf);
 	aArray.push(a);
   }
-  
-  return {txId:txId, 
+
+
+  return {txId:txId,
 		  flags:flags,
 		  opCode:opCode,
 		  isQuery:(flags&0x8000)==0,
@@ -71,9 +94,9 @@ function parseDnsMessage(bytes) {
 		  recursionDesired:(flags&0x100)!=0,
 		  recursionAvailable:(flags&0x80)!=0,
 		  rCode:(flags&0xf),
-		  questionCnt:questionCnt, 
-		  answerCnt:answerRRCnt, 
-		  authorityCnt:authorityRRCnt, 
+		  questionCnt:questionCnt,
+		  answerCnt:answerRRCnt,
+		  authorityCnt:authorityRRCnt,
 		  additionalCnt:additionalRRCnt,
 		  queries:qArray,
 		  answers:aArray
@@ -81,13 +104,12 @@ function parseDnsMessage(bytes) {
 }
 
 function getAnswers(buf) {
-  var ret = new Object();
+  var ret = new AnswerSection();
   ret.rdata = "";
-  
+
   if((offset + 8) > limit) return ret;
 
-  var p = getShort(buf, offset)&0x3fff; offset += 2;
-
+  var p = (getShort(buf, offset)&0x3fff) + base; offset += 2;
   ret.name = getName(p, buf, 0);
   ret.type  = getShort(buf, offset); offset += 2;
   ret.class = getShort(buf, offset); offset += 2;
@@ -95,7 +117,6 @@ function getAnswers(buf) {
   var rdLen = getShort(buf, offset); offset += 2;
 
   if(offset + rdLen > limit) return ret;
-  
   if(ret.type == 1) //IPv4
 	ret.rdata = getIpv4Str(offset, buf, rdLen);
   else if(ret.type == 5) //CNAME
@@ -103,7 +124,7 @@ function getAnswers(buf) {
   else if(ret.type == 28) //IPv6
 	ret.rdata = getIpv6Str(offset, buf, rdLen);
 
-  offset += rdLen;  
+  offset += rdLen;
   return ret;
 }
 
@@ -116,10 +137,11 @@ function getQueries(buf) {
 	for(var i=offset; i<offset+slen;i++) {
 	  label.push(buf[i] >= 0x20 && buf[i] < 0x7f ? String.fromCharCode(buf[i]) : '.');
 	}
-	question.push(label.join("")); 
+	question.push(label.join(""));
 	offset += slen;
   }
-  var ret = new Object();
+
+  var ret = new QuestionSection();
   if(offset <= limit) {
 	ret.qType  = getShort(buf, offset); offset += 2;
 	ret.qClass = getShort(buf, offset); offset += 2;
@@ -135,15 +157,15 @@ function getName(pos, buf, depth) {
 	return "Name contains pointer that loops";
   }
   while((pos < limit)) {
-	var slen = getByte(buf, pos); pos++; //(buf[(pos)]&0xff); pos++;
+	var slen = getByte(buf, pos); pos++;
 	var label = [];
 	if(slen == 0) {
 	  break;  // stop processing after last label
 	} else if ((slen&0xc0)!=0) { //referred label in compressed message
-	  newPos = (((slen<<8)&0x3f00) | getByte(buf, pos));
+	  var newPos = (((slen<<8)&0x3f00) | getByte(buf, pos)) + base;
 	  if(newPos < limit) {
 		var nDepth = depth + 1;
-		ret.push(getName(newPos, buf, nDepth));		  
+		ret.push(getName(newPos, buf, nDepth));
 	  }
 	  break;
 	}
@@ -151,18 +173,18 @@ function getName(pos, buf, depth) {
 	  var b = getByte(buf, i);
 	  label.push(b >= 0x20 && b < 0x7f ? String.fromCharCode(b) : '.');
 	}
-	ret.push(label.join("")); 
+	ret.push(label.join(""));
 	pos += slen;
   }
   return ret.join(".");
 }
-  
+
 function getIpv4Str(pos, buf, len) {
   var ret = [];
   for(i=pos; i<(pos+len) && i<limit;i++) {
 	ret.push(getByte(buf, i).toString());
   }
-  return ret.join(".");  
+  return ret.join(".");
 }
 
 function getIpv6Str(pos, buf, len) {
@@ -170,11 +192,6 @@ function getIpv6Str(pos, buf, len) {
   for(i=pos; i<(pos+len) && i<limit;i+=2) {
 	ret.push(pad(getByte(buf,i).toString(16),2) + pad(getByte(buf,i+1).toString(16),2));
   }
-  return ret.join(":");  
+  return ret.join(":");
 }
 
-function pad(num, size) {
-  var s = ""+num;
-  while (s.length < size) s = "0" + s;
-  return s;
-}
--- scripts/dns/pcap-utils.js
+++ scripts/dns/pcap-utils.js
@@ -1,140 +1,74 @@
-/*
-	todo: 1) get rid of captureLen
-*/
-function PcapPacket(bytes) {
-  this.data = [];
-  this.size = 0;
-  var incl_len = getIntReverse(bytes, 8);
-  
-  if(bytes.length > 0 && incl_len > 0 && incl_len <= bytes.length) {
-	this.data = bytes;
-	this.size = incl_len + 16; //+pcap header len;	
-  }
-  
-  this.networkType = -1;
-  if(this.size >= 30)
-	this.networkType = getShort(this.data, 28);
-
-  this.transportType = -1; 
-  if(this.networkType==0x86dd  && this.size>=37) { //IPv6
-	this.transportType = getByte(this.data, 36);
-  } else if (this.networkType==0x800  && this.size>=40) {  //IPv4
-	this.transportType = getByte(this.data, 39); 
-  }
-  
-  this.payloadOffset = getPayloadOffset(this.networkType, this.transportType, this.size, this.data);
+function PcapPacket(bytes, captureLen, network, transport) {
+  this.data = bytes;
+  this.size = captureLen+16; //+tcpdump header: ts_sec, ts_usec, incl_len, orig_len
+  this.payloadOffset = computePayloadOffset(network, transport, this.data, this.size);
 
   /***************** public methods *****************/
 
-  this.getSize = function() {
-	return this.size;
+  this.getPayloadOffset = function() {
+	return this.payloadOffset;
   }
-  
-  this.getPayload = function() {
-	var ret = [];
-	if(this.hasData() == true)
-	  ret = getBytes(this.data, this.payloadOffset, this.size);
-	return ret;
-  }
-  
+
   this.hasData = function() {
 	var ret = false;
 	if(this.size > 0)
 		ret = (this.size-this.payloadOffset) > 0;
 	return ret;
-  } 
-  
+  }
+
   this.toStr = function() {
 	return ("size: " + this.size.toString()
-			  + " payload offset: " + this.payloadOffset.toString()
 			  + " has data: " + this.hasData().toString()
+			  + " payload offset: " + this.payloadOffset.toString()
 			  + " payload size: " + (this.size-this.payloadOffset).toString()
-			  + " network type: " + (this.networkType==0x86dd?"IPv6":"IPv4")
-			  + " transport type: " + (this.transportType==6?"TCP":"UDP")
-			  + " srcPort: " + this.getSrcPort().toString()
-			  + " dstPort: " + this.getDstPort().toString()
 			 );
   }
-  
-  this.getNetworkType = function() {
-	return this.networkType;
-  }
-  
-  this.getTransportType = function() {
-	return this.transportType;
-  }
 
-  this.getSrcPort = function() {
-	var trpHdrOffset = 
-		16 + 14 + 
-		(this.networkType==0x8100?2:0) +	//VLAN Q-tags
-		(this.networkType==0x86dd?40:20)	//network header
-	;
-	
-	return getShort(this.data, trpHdrOffset);
-  }
-
-  this.getDstPort = function() {
-	var trpHdrOffset = 
-		16 + 14 + 
-		(this.networkType==0x8100?2:0) +	//VLAN Q-tags
-		(this.networkType==0x86dd?40:20)	//network header
-	;
-	
-	return getShort(this.data, trpHdrOffset+2);
-  }
-
-
-  /***************** private functions *****************/
-
-  function getPayloadOffset(nwType, trType, size, buf) {	
+  function computePayloadOffset(network, transport, buf, size) {
+	var trHdrLen = 8; 	//UDP header is 8 bytes
 	var nwHdrLen = 20;  //default IPv4 hdr, length 20 bytes
 	var vlanFlagsLen = 0;
-	var trlHdrLen = 8; 	//UDP header is 8 bytes
 
-	if(nwType < 0 || size <= 0) {
-	  return 0;
-	} else if(nwType == 0x86dd) { //IPv6
-	  nwHdrLen = 40;			//IPV6 network layer header is 40 bytes
+	if(network == 0xdd86) {  //IPv6
+	  nwHdrLen = 40;		//IPV6 network layer header is 40 bytes
 	}
 
-	if (nwType == 0x8100) {//VLAN (https://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml)
+	if (network == 0x81) {//VLAN (https://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml)
 	  vlanFlagsLen = 2;
 	}
-
-	if(size >= 65 && trType == 6) {	//TCP
-	  trlHdrLen = (((buf[62]>>4)&0xf) * 4);
+	if(size >= 65 && transport == 6) {	//TCP
+	  trHdrLen = (((buf[62]>>4)&0xf) * 4);
 	}
-	
-	return ( 
+
+	return (
 	  16  			 //tcpdump header: ts_sec, ts_usec, incl_len, orig_len
 	  + 14 			 //link-layer header: srcMac 6, dstMac 6, nwHdrType 2 bytes
 	  + vlanFlagsLen //vlan labels
-	  + nwHdrLen 	 //network layer header	
-	  + trlHdrLen    //transport layer header
-	);	
+	  + nwHdrLen 	 //network layer header
+	  + trHdrLen    //transport layer header
+	);
   }
-
 }
 
 /******************* public helper functions *****************/
 
 function getByte(buf, pos) {
-  return buf.length > pos ? buf[pos]&0xff : 0;
+  //return buf.length > pos ? buf[pos]&0xff : 0;
+  return buf[pos]&0xff;
 }
 
 function getBytes(buf, from, to) {
   var ret = [];
-  if(from > buf.length || to > buf.length || from < 0 || to < 0) return ret;
+  //if(from > buf.length || to > buf.length || from < 0 || to < 0) return ret;
   for(var i=0; i+from<to; i++) {
 	ret.push(buf[from+i]&0xff);
-  }  
+  }
   return ret;
 }
 
 function bytesToStr(byteArray, from, to) {
   var ret = [];
-  if(from > byteArray.length || to > byteArray.length || from < 0 || to < 0) return ret;
+  //if(from > byteArray.length || to > byteArray.length || from < 0 || to < 0) return ret;
   for(var i=0; from+i<to; i++) {
 	if(i%16==0) ret.push("\n");
 	ret.push(pad((byteArray[from+i]&0xff).toString(16),2) + " ");
@@ -143,29 +77,31 @@ function bytesToStr(byteArray, from, to) {
 }
 
 function getShort(buf, pos) {
-  return buf.length > pos+1 ? (((buf[pos]<<8)&0xff00) | (buf[pos+1]&0x00ff)) : 0;
+  //return buf.length > pos+1 ? (((buf[pos]<<8)&0xff00) | (buf[pos+1]&0x00ff)) : 0;
+  return (((buf[pos]<<8)&0xff00) | (buf[pos+1]&0x00ff));
 }
 
 function getInt(buf, pos) {
-  if(buf.length <= pos+3) return 0;
-  return ( ((buf[pos]<<24)&0xff000000) 
-			 | ((buf[pos+1]<<16)&0xff0000) 
-			 | ((buf[pos+2]<<8)&0xff00) 
-			 | (buf[pos+3]&0xff) 
-			);  
+//  if(buf.length <= pos+3) return 0;
+  return ( ((buf[pos]<<24)&0xff000000)
+			 | ((buf[pos+1]<<16)&0xff0000)
+			 | ((buf[pos+2]<<8)&0xff00)
+			 | (buf[pos+3]&0xff)
+			);
 }
 
 function getShortReverse(buf, pos) {
-  return buf.length > pos+1 ? (((buf[pos+1]<<8)&0xff00) | (buf[pos]&0x00ff)) : 0;
+  //return buf.length > pos+1 ? (((buf[pos+1]<<8)&0xff00) | (buf[pos]&0x00ff)) : 0;
+  return (((buf[pos+1]<<8)&0xff00) | (buf[pos]&0x00ff));
 }
 
 function getIntReverse(buf, pos) {
-  if(buf.length <= pos+3) return 0;
-  return ( ((buf[pos+3]<<24)&0xff000000) 
-			 | ((buf[pos+2]<<16)&0xff0000) 
-			 | ((buf[pos+1]<<8)&0xff00) 
-			 | (buf[pos]&0xff) 
-			);    
+//  if(buf.length <= pos+3) return 0;
+  return ( ((buf[pos+3]<<24)&0xff000000)
+			 | ((buf[pos+2]<<16)&0xff0000)
+			 | ((buf[pos+1]<<8)&0xff00)
+			 | (buf[pos]&0xff)
+			);
 }
 
 function pad(num, size) {
@@ -173,6 +109,3 @@ function pad(num, size) {
   while (s.length < size) s = "0" + s;
   return s;
 }
-
-
-
